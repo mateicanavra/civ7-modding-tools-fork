f35ce7f3e48d0c30274cc7b907e6b98e79ba42f7
izica <artemiztomska@gmail.com>
2025-03-08 16:24:21 +0700

    compiled

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _createStarExport(obj) { Object.keys(obj) .filter((key) => key !== "default" && key !== "__esModule") .forEach((key) => { if (exports.hasOwnProperty(key)) { return; } Object.defineProperty(exports, key, {enumerable: true, configurable: true, get: () => obj[key]}); }); }var _constants = require('./constants'); _createStarExport(_constants);
+var _core = require('./core'); _createStarExport(_core);
+var _files = require('./files'); _createStarExport(_files);
+var _builders = require('./builders'); _createStarExport(_builders);
+var _types = require('./types'); _createStarExport(_types);
+var _utils = require('./utils'); _createStarExport(_utils);
+var _localizations = require('./localizations'); _createStarExport(_localizations);
+var _nodes = require('./nodes'); _createStarExport(_nodes);
+var _presets = require('./presets'); _createStarExport(_presets);
+//# sourceMappingURL=index.js.map
---
+++ dist/index.mjs
@@ -0,0 +1,10 @@
+export * from "./constants";
+export * from "./core";
+export * from "./files";
+export * from "./builders";
+export * from "./types";
+export * from "./utils";
+export * from "./localizations";
+export * from "./nodes";
+export * from "./presets";
+//# sourceMappingURL=index.mjs.map
