89d0c910bad0a7128b0ef0a2e5e81cf5d7671d1b
Matei Canavra <tools@matei.work>
2025-08-24 02:24:47 -0400

    move it

--- mods/mod-swooper-dacia/src/shared/modifiers/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Shared modifiers that are truly generic and not tied to specific entities.
- */
-
-import { ModifierBuilder } from "@mateicanavra/civ7-sdk";
-
-// Collection of shared modifiers
-export const sharedModifiers: ModifierBuilder[] = []; 
