0b7570f6cec805f8633e64661f017fe72e3b9db6
Matei <tools@matei.work>
2026-01-03 03:30:34 -0500

    fix(mod-swooper-maps): provide riftsFollowPlates directionality

--- mods/mod-swooper-maps/src/maps/swooper-earthlike.ts
+++ mods/mod-swooper-maps/src/maps/swooper-earthlike.ts
@@ -47,6 +47,7 @@ const config = {
             interplay: {
               windsFollowPlates: 0.3,
               currentsFollowWinds: 0.6,
+              riftsFollowPlates: 0.75,
             },
             hemispheres: {
               southernFlip: true,
