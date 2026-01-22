d0cd9d051511feea0d9439bf3b0ba8fa28e1709f
Matei <tools@matei.work>
2026-01-21 20:49:32 -0500

    placement: project landmass regions via map artifact

--- mods/mod-swooper-maps/src/recipes/standard/stages/placement/index.ts
+++ mods/mod-swooper-maps/src/recipes/standard/stages/placement/index.ts
@@ -1,8 +1,8 @@
 import { Type, createStage } from "@swooper/mapgen-core/authoring";
-import { derivePlacementInputs, placement } from "./steps/index.js";
+import { derivePlacementInputs, placement, plotLandmassRegions } from "./steps/index.js";
 
 export default createStage({
   id: "placement",
   knobsSchema: Type.Object({}, { additionalProperties: false }),
-  steps: [derivePlacementInputs, placement],
+  steps: [derivePlacementInputs, plotLandmassRegions, placement],
 } as const);
