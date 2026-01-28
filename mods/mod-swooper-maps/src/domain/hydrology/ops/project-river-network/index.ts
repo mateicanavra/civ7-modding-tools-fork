ad7fc62afca6a41dda767eaef5f2e4698b5ec252
Matei <tools@matei.work>
2026-01-28 16:16:47 -0500

    hydrology(hydrography): add geometry fields + physics strategy

--- mods/mod-swooper-maps/src/domain/hydrology/ops/project-river-network/index.ts
+++ mods/mod-swooper-maps/src/domain/hydrology/ops/project-river-network/index.ts
@@ -1,13 +1,12 @@
 import { createOp } from "@swooper/mapgen-core/authoring";
 import ProjectRiverNetworkContract from "./contract.js";
-import { defaultStrategy } from "./strategies/index.js";
+import { defaultStrategy, physicsStrategy } from "./strategies/index.js";
 
 const projectRiverNetwork = createOp(ProjectRiverNetworkContract, {
-  strategies: { default: defaultStrategy },
+  strategies: { default: defaultStrategy, physics: physicsStrategy },
 });
 
 export type * from "./types.js";
 export type * from "./contract.js";
 
 export default projectRiverNetwork;
-
